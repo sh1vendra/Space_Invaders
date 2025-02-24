58189119e84adbbfc7a77558eae90fb8c706e254
Ted Lehr <t_l81@txstate.edu>
2025-02-24 12:40:10 -0600

    Added invokeLater to ensure thread safety

--- src/spaceinvaders/Main.java
+++ src/spaceinvaders/Main.java
@@ -1,14 +1,18 @@
 package spaceinvaders;
 
 import javax.swing.JFrame;
+import javax.swing.*;
+
 
 public class Main {
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Space Invaders with Images");
-        SpaceInvadersUI game = new SpaceInvadersUI();
-        frame.add(game);
-        frame.setSize(600, 700);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame("Space Invaders with Images");
+            SpaceInvadersUI game = new SpaceInvadersUI();
+            frame.add(game);
+            frame.setSize(600, 700);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setVisible(true);
+        });
     }
 }
