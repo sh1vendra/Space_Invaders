645ba27b48c43c9936340b4ba383909d12e4d055
aditya_baniya <itsadityabaniya@gmail.com>
2025-03-04 02:23:40 -0600

    changed music menu

--- src/spaceinvaders/MenuBar/MusicMenu.java
+++ src/spaceinvaders/MenuBar/MusicMenu.java
@@ -1,29 +1,36 @@
 import javax.sound.sampled.*;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+
 import java.io.*;
 
 package spaceinvaders.MenuBar;
 
-<<<<<<< HEAD
 // create a branch for MusicMenu completed.
-=======
 // create a branch for MusicMenu completed
->>>>>>> aditya_branch
 
 public class MusicMenu {
     private static Clip clip;
 
-    public static void playMusic(String filename) {
-        stopMusic();
-        try {
-            File file = new File("resources/music/" + filename);
-            AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
-            clip = AudioSystem.getClip();
-            clip.open(audioStream);
-            clip.loop(Clip.LOOP_CONTINUOUSLY);
-            clip.start();
-        } catch (Exception e) {
-            System.out.println("Error: Unable to play music. " + e.getMessage());
-        }
+    public static JMenu createMusicMenu() {
+        JMenu musicMenu = new JMenu("Music");
+
+        JMenuItem music1 = new JMenuItem("Music 1");
+        JMenuItem music2 = new JMenuItem("Music 2");
+        JMenuItem music3 = new JMenuItem("Music 3");
+        JMenuItem customMusic = new JMenuItem("Custom Music");
+
+        music1.addActionListener(e -> playMusic("music1.wav"));
+        music2.addActionListener(e -> playMusic("music2.wav"));
+        music3.addActionListener(e -> playMusic("music3.wav"));
+        customMusic.addActionListener(e -> playCustomMusic());
+
+        musicMenu.add(music1);
+        musicMenu.add(music2);
+        musicMenu.add(music3);
+        musicMenu.add(customMusic);
+
+        return musicMenu;
     }
 
     public static void playCustomMusic() {
